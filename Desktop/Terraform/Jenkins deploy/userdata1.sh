380b1d3b9da25cc14fbd55c5be300cdf5af43115
Ved96 <87043408+Ved96@users.noreply.github.com>
2023-01-16 12:42:46 +0530

    Delete userdata1.sh

--- Desktop/Terraform/Jenkins deploy/userdata1.sh
+++
@@ -1,7 +0,0 @@
-
-sudo yum update –y
-sudo amazon-linux-extras install epel -y
-sudo yum install java -1.8* -y
-sudo yum install wget -y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.55/bin/apache-tomcat-9.0.55.tar.gz
-sudo tar -zvxf apache-tomcat-9.*
